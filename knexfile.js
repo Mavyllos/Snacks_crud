3f549004049684f92771134f48d42585627f621d
Summer Barclay <evil_lyns@yahoo.com>
2017-04-13 10:50:15 -0700

    Updated knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -10,5 +10,15 @@ module.exports = {
     seeds: {
       directory: path.join(__dirname, 'db', 'seeds')
     }
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: path.join(__dirname, 'db', 'migrations')
+    },
+    seeds: {
+      directory: path.join(__dirname, 'db', 'seeds')
+    }
   }
 }
